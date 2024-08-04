cc5cc0b58da8132b2635129b4fafd944265ec663
Van Duy <vanduy352003@gmail.com>
2024-08-04 10:40:03 +0700

    add frontend project

---
+++ book-network/src/main/java/com/duy/book/config/OpenApiConfig.java
@@ -0,0 +1,54 @@
+package com.duy.book.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Duy",
+                        email = "123",
+                        url = "123"
+                ),
+                description = "OpenApi documentation for Spring security",
+                title = "OpenApi specification - Duy",
+                version = "1.0",
+                license = @License(
+                        name = "License name",
+                        url = "123"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local Env",
+                        url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "Prod Env",
+                        url = "https://tuong-trung"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
